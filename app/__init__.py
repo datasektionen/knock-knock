8e54aa70d4fe8845b891324d088b7d1f72c06f7e
Ludvig Kindberg <lki@kth.se>
2018-12-05 00:19:36 +0100

    Fixed the endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,77 @@
-from flask import Flask,render_template
+import json
+import requests
+from flask import Flask,render_template, request, abort
 from random import randint
+
 # from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-@app.route('/api/')
+def authenticate(permission):
+    def foo(f):
+        def decorated_function():
+            # token = request.headers.get("api_key")
+            token = "luddz-_qBFhK1VDoQo16AxTUBahsirHogH-2rYXp8q5foYBsk"
+            url = f'https://pls.datasektionen.se/api/token/{token}/knockknock/{permission}/'
+            response = requests.get(url)
+            allowed = response.json()
+            if not allowed:
+                abort(403)
+            return f()
+
+        return decorated_function
+    return foo
+
+@app.route('/', methods=['GET'])
 def hello_world():
-    return "Hello home"
+    return "Who's there?"
+
+@app.route('/', methods=['POST'])
+@authenticate("write")
+def hello_worldx():
+    return "POSTING LIKE A BOSS"
 
 # This should CREATE a new event
-@app.route('/api/event/') #Post
+@authenticate("write")
+@app.route('/api/event/', methods=['POST']) #Post
 def getEvents():
     return "getEvents"
 
 # This should GENERATE a list of the persons on the specified event
-@app.route('/api/event/<string:event>/') #GET
+@authenticate("read")
+@app.route('/api/event/<string:event>/', methods=['GET']) #GET
 def getPresent():
     return "getPresent"
 
 #This should generate if user has changed in/out
 # GET should generate True or False
-@app.route('/api/event/<string:event>/user/<ustring:ser>/') #PUT/GET
-def handleUser():
+@app.route('/api/event/<string:event>/user/<string:user>/', methods=['PUT,GET']) #GET
+@authenticate("read")
+def handleUserGet():
+    return "handleUser"
+
+#This should generate if user has changed in/out
+# PUT should change the status of the user.
+@app.route('/api/event/<string:event>/user/<string:user>/', methods=['PUT']) #PUT
+@authenticate("write")
+def handleUserPut():
     return "handleUser"
 
 #This should generate if user(with UGID) has changed in/out
 # GET should generate True or False
-@app.route('/api/event/<string:event>/ugid/<string:ugid>/') #PUT/GET
-def handleUgid():
-    return "handleUgid"
+@app.route('/api/event/<string:event>/ugid/<string:ugid>/',methods=['GET']) #GET
+@authenticate("read")
+def handleUgidGet():
+    return "handleUgidGet"
 
-@app.route('/knock/')
-def knock():
-    return "knock"
+#This should generate if user(with UGID) has changed in/out
+# PUT should change the status of the current user
+@app.route('/api/event/<string:event>/ugid/<string:ugid>/',methods=['PUT']) #PUT
+@authenticate("write")
+def handleUgidPut():
+    return "handleUgidPut"
 
-@app.route('/knock/<string:joke>/')
-def getJoke(joke):
+@app.route('/knock/', methods=['GET'])
+def getJokes():
     jokes = [ "This is a joke", "Another joke", "A third joke, and now to the awsome"]
     randomNumber = randint(0,len(jokes)-1)
     joke = jokes[randomNumber]
